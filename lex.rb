8444cf9a2c5048910cd844a6c030a955a9445745
twinbird <ixa2063@gmail.com>
2016-01-31 00:30:08 +0900

    トークン取得メソッドと解析直後の状態を持つアクセサを追加.テストも合わせて変更.

--- lex.rb
+++ lex.rb
@@ -1,50 +1,57 @@
 class Lex
   attr_reader :tokens
+  attr_reader :parse_after_snapshot
+
   def initialize(code)
     @code = code
-    @idx = 0
+    @current_parse_idx = 0
     @tokens = []
     parse
+    @parse_after_snapshot = @tokens.dup
+  end
+
+  def get_token
+    @tokens.shift
   end
 
   private
 
   def num_parse
     num_token = ""
-    while (@code[@idx] =~ /\d/)
-      num_token += @code[@idx]
-      @idx += 1
+    while (@code[@current_parse_idx] =~ /\d/)
+      num_token += @code[@current_parse_idx]
+      @current_parse_idx += 1
     end
     num_token
   end
 
   def parse
-    while (@idx < @code.length)
-      case @code[@idx]
+    while (@current_parse_idx < @code.length)
+      case @code[@current_parse_idx]
       when "+" then
         @tokens << "+"
-        @idx += 1
+        @current_parse_idx += 1
       when "-" then
         @tokens << "-"
-        @idx += 1
+        @current_parse_idx += 1
       when "/" then
         @tokens << "/"
-        @idx += 1
+        @current_parse_idx += 1
       when "*" then
         @tokens << "*"
-        @idx += 1
+        @current_parse_idx += 1
       when "(" then
         @tokens << "("
-        @idx += 1
+        @current_parse_idx += 1
       when ")" then
         @tokens << ")"
-        @idx += 1
+        @current_parse_idx += 1
       when /\d/ then
         @tokens << num_parse
       when /\s/ then
-        @idx += 1
+        @current_parse_idx += 1
       else
-        raise "unknown char" + @code[@idx] + ":" + @idx.to_s
+        raise "unknown char" + @code[@current_parse_idx] + ":" + @current_parse_idx.to_s
       end
     end
   end
--- test.rb
+++ test.rb
@@ -6,15 +6,38 @@ class TestLexer < Test::Unit::TestCase
     @program = "12+3+5/(1+3)*(6-2)" # => 20
     @space_sperate_program = "12 + 3 + 5 / (1 + 3) * (6 - 2)" # => 20
     @parsed_lexis = ["12","+","3","+","5","/","(","1","+","3",")","*","(","6","-","2",")"]
+
+    @program_parsed_lex = Lex.new(@program)
+    @space_separate_program_parsed_lex = Lex.new(@space_sperate_program)
   end
 
   def test_parse
-    lex = Lex.new(@program)
-    assert_equal(lex.tokens, @parsed_lexis)
+    assert_equal(@program_parsed_lex.tokens, @parsed_lexis)
+    assert_equal(@program_parsed_lex.parse_after_snapshot, @parsed_lexis)
+    assert_equal(@space_separate_program_parsed_lex.parse_after_snapshot, @parsed_lexis)
   end
 
   def test_space_sperate_program_parse
-    lex = Lex.new(@space_sperate_program)
-    assert_equal(lex.tokens, @parsed_lexis)
+    assert_equal(@space_separate_program_parsed_lex.tokens, @parsed_lexis)
+    assert_equal(@space_separate_program_parsed_lex.parse_after_snapshot, @parsed_lexis)
   end
+
+  def test_get_token_first
+    assert_equal(@program_parsed_lex.get_token, @parsed_lexis.first)
+    assert_equal(@program_parsed_lex.tokens, @parsed_lexis[1..-1])
+    assert_equal(@program_parsed_lex.parse_after_snapshot, @parsed_lexis)
+  end
+
+  def test_get_token_last
+    (@parsed_lexis.size - 1).times {|i| @program_parsed_lex.get_token}
+    assert_equal(@program_parsed_lex.get_token, @parsed_lexis.last)
+    assert_equal(@space_separate_program_parsed_lex.parse_after_snapshot, @parsed_lexis)
+  end
+
+  def test_get_token_for_empty
+    @parsed_lexis.size.times {|i| @program_parsed_lex.get_token}
+    assert_equal(@program_parsed_lex.get_token, nil)
+    assert_equal(@space_separate_program_parsed_lex.parse_after_snapshot, @parsed_lexis)
+  end
+
 end
