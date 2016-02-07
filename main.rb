ecce09985ed423cdfafa4d93cc001cb7466389ef
twinbird <ixa2063@gmail.com>
2016-02-08 00:01:30 +0900

    出力フォーマットを整えた

--- main.rb
+++ main.rb
@@ -1,18 +1,32 @@
 require './evaluator'
 
-#program = "12 + 3 + 5 / (1 + 3) * (6 - 2)" # => 20
+# Test sample
 program = "12+3+5/(1+3)*(6-2)" # => 20
-#program = "(1+2)*(3-(4/5))"
 
 program = ARGV[0] unless ARGV[0].nil?
 
+puts "Program"
+puts "-----------------------------------"
 p program
+puts "-----------------------------------"
+puts ""
 
+puts "Tokens"
+puts "-----------------------------------"
 lex = Lex.new(program)
 p lex.tokens
+puts "-----------------------------------"
+puts ""
 
+puts "AST"
+puts "-----------------------------------"
 parser = Parser.new(lex)
 parser.printAST
+puts "-----------------------------------"
+puts ""
 
+puts "Result"
+puts "-----------------------------------"
 evaluator = Evaluator.new(parser.ast_root)
 p evaluator.eval
+puts "-----------------------------------"
